3ef5914439401f588576958aa844617b97046b59
Aeluwas <serenity@exscape.org>
2026-03-17 04:18:31 +0100

    Mod config: SliderRange error checking

--- Config/ConfigAttributes.cs
+++ Config/ConfigAttributes.cs
@@ -7,11 +7,28 @@ public class ConfigSectionAttribute(string name) : Attribute
 }
 
 [AttributeUsage(AttributeTargets.Property)]
-public class SliderRangeAttribute(double min, double max, double step = 1.0) : Attribute
+public class SliderRangeAttribute : Attribute
 {
-    public double Min { get; } = min;
-    public double Max { get; } = max;
-    public double Step { get; } = step;
+    public double Min { get; }
+    public double Max { get; }
+    public double Step { get; }
+
+    public SliderRangeAttribute(double min, double max, double step = 1.0)
+    {
+        if (min > max)
+        {
+            throw new ArgumentException($"SliderRange: Min ({min}) cannot be greater than Max ({max}).");
+        }
+
+        if (step <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(step), "SliderRange: Step must be greater than 0.");
+        }
+
+        Min = min;
+        Max = max;
+        Step = step;
+    }
 }
 
 [AttributeUsage(AttributeTargets.Property)]
